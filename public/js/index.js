3be27b831678d3a561e4c236f92488ee22cefacb
Abdel <juniorperezpy@gmail.com>
2020-07-17 16:31:51 -0400

    Add eventListenet to save button

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,19 @@
-console.log('hello');
+let firestore = firebase.firestore();
+
+const outputHeader = document.querySelector('#hotDogOutput');
+const inputTextField = document.querySelector('#latestHotDogStatus');
+const saveButton = document.querySelector('#saveButton');
+
+const docRef = firestore.doc('samples/sandwichData');
+
+saveButton.addEventListener('click', function() {
+  const textToSave = inputTextField.value;
+  console.log(`I'm going to save ${textToSave} to Firestore`);
+  docRef.set({
+    hotDogStatus: textToSave
+  }).then(()=> {
+    console.log("Status saved!");
+  }).catch(error => {
+    console.log("Got an error: ", error);
+  });
+});
